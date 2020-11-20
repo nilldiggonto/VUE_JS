35ec5de67a174932092e10f9ae99e33a9dfea4c7
nilldiggonto <nilldiggonto@gmail.com>
2020-11-21 04:44:47 +0600

    Basic Instance

---
+++ Intro_to_vue/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        message: 'Something Inside',
+        value:10,
+        imgSrc: 'https://images.unsplash.com/photo-1556954535-6b941cdc6b1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        link: 'https://unsplash.com/photos/cWVDzxXpx3k',
+    }
+}) 
