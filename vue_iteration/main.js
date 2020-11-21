c6b15882e5d02f54c82341b34e9e32521fd3952b
nilldiggonto <nilldiggonto@gmail.com>
2020-11-21 06:34:31 +0600

    Vue Iter

---
+++ vue_iteration/main.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el:'#app',
+    data:
+    {
+        'title':'Vue Iteration',
+        'games':
+        [
+            {
+                name:'Limbo',
+                availability:'yes',
+                multyplayer:'no',
+                
+            },
+            {
+                name:'Inside',
+                availability:'yes',
+                multyplayer:'no',
+                
+            },
+            {
+                name:'Hades',
+                availability:'yes',
+                multyplayer:'no',
+                
+            },
+        ] 
+    }
+}) 
